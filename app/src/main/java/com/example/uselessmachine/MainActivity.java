8dad87a7a62186a224dec4fde243dbc1285018d7
per6 <gippy713@gmail.com>
2019-09-04 13:33:23 -0700

    Loading Bar was added

--- app/src/main/java/com/example/uselessmachine/MainActivity.java
+++ app/src/main/java/com/example/uselessmachine/MainActivity.java
@@ -9,7 +9,9 @@ import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.ProgressBar;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +19,11 @@ public class MainActivity extends AppCompatActivity {
     private Button button;
     private Switch theSwitch;
     private ConstraintLayout background;
+    private Button buttonDo;
+    private ProgressBar loading;
+    private TextView text;
+    private int color;
+    private int white;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +31,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         wireWidgets();
         setListeners();
-        int color = Color.rgb(255,5,5);
+        color = Color.rgb(255,5,5);
+        white = Color.rgb(255,255,255);
         button.setBackgroundColor(color);
     }
     public void setListeners() {
@@ -33,23 +41,28 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 new CountDownTimer(10000, 1000) {
                     int tickNumber = 11;
+
                     @Override
 
-                    // cancels the timer if switch is turned off before automatically being turned
-                    // off
 
-
-
-                        public void onTick (long l){
+                    public void onTick(long l) {
 
                         tickNumber--;
-                            button.setText(tickNumber + "");
+                        if (tickNumber % 2 != 0) {
+                            button.setBackgroundColor(color);
+                            background.setBackgroundColor(white);
+                        } else {
+                            button.setBackgroundColor(white);
+                            background.setBackgroundColor(color);
                         }
+                        button.setText(tickNumber + "");
+                    }
 
 
                     @Override
                     public void onFinish() {
-                        int oniichan = 0/0;
+                        int oniichan = 0 / 0;
+                        // finish();
                     }
                 }.start();
             }
@@ -64,9 +77,8 @@ public class MainActivity extends AppCompatActivity {
                     // cancels the timer if switch is turned off before automatically being turned
                     // off
                     public void onTick(long l) {
-                        if(!theSwitch.isChecked())
-                        {
-                          cancel();
+                        if (!theSwitch.isChecked()) {
+                            cancel();
                         }
                     }
 
@@ -82,6 +94,36 @@ public class MainActivity extends AppCompatActivity {
 //                }
             }
         });
+        buttonDo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                button.setVisibility(View.INVISIBLE);
+                theSwitch.setVisibility(View.INVISIBLE);
+                buttonDo.setVisibility(View.INVISIBLE);
+                loading.setVisibility(View.VISIBLE);
+                text.setVisibility(View.VISIBLE);
+                new CountDownTimer(10000,1000)
+                {
+                    int x = 0;
+                    @Override
+                    public void onTick(long l) {
+                        loading.setProgress(x);
+                        x+=10;
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        button.setVisibility(View.VISIBLE);
+                        theSwitch.setVisibility(View.VISIBLE);
+                        buttonDo.setVisibility(View.VISIBLE);
+                        loading.setVisibility(View.INVISIBLE);
+                        text.setVisibility(View.INVISIBLE);
+                    }
+                }.start();
+
+            }
+        });
+
     }
 
     public void wireWidgets()
@@ -89,6 +131,9 @@ public class MainActivity extends AppCompatActivity {
             button = findViewById(R.id.button_mian_destruct);
             theSwitch = findViewById(R.id.switch_main_useless);
             background = findViewById(R.id.constraint_main_layout);
+            buttonDo = findViewById(R.id.button_main_loadingBar);
+            loading = findViewById(R.id.progressBar_main_loading);
+            text = findViewById((R.id.textView_main_update));
 
         }
     }
